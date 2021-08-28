3e9751058fe7d494e6170067f24d361bc543d501
Jeremias Santos <jeremiassnts3@gmail.com>
2021-08-27 21:56:21 -0300

    feat: creating user

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body
+
+  if (users.some(u => u.username === username)) {
+    return response.status(400).json({
+      "error": "Username unavailable"
+    })
+  }
+
+  const newuser = {
+    id: uuidv4(),
+    name: name,
+    username: username,
+    todos: []
+  }
+  users.push(newuser)
+
+  return response.status(201).json(newuser)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
