739fa1e6e375a93573c2b7aefd181bad416f9de0
Jeremias Santos <jeremiassnts3@gmail.com>
2021-08-27 22:03:32 -0300

    feat: get todos by user

--- src/index.js
+++ src/index.js
@@ -11,7 +11,17 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(u => u.username === username)
+  if (!user) {
+    return response.status(404).json({
+      "error": "Not authorized"
+    })
+  }
+
+  request.body.user = user
+  return next()
 }
 
 app.post('/users', (request, response) => {
@@ -35,7 +45,8 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { todos } = request.body.user
+  return response.json([...todos])
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
